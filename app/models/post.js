ce6ddfd5cf19891d4065fa5259bef434724f8327
mlendale <ml.endale@gmail.com>
2016-10-27 07:16:46 +0200

    Add form validation to post form

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,17 @@
 import DS from 'ember-data';
 import { validator, buildValidations } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+  content: [
+    validator('presence', true),
+    validator('length', {
+      max: 100000
+    })
+  ]
+});
+
+
+export default DS.Model.extend(Validations,{
   content: DS.attr('string'),
   created_at: DS.attr('date'),
   user: DS.belongsTo('user', { async: true })
