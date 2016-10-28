b4fbe356134cc2204d386102ab632396e7b59ff2
mlendale <ml.endale@gmail.com>
2016-10-28 10:13:05 +0200

    Add a post destroy action

--- app/components/single-post.js
+++ app/components/single-post.js
@@ -5,7 +5,14 @@ const { inject: { service }, Component } = Ember;
 export default Component.extend({
   session:     service('session'),
   currentUser: service('current-user'),
-
+  actions :{
+  	deletePost(){
+  	  let deletedPost = this.get('post');
+  	  console.log(deletedPost.id)
+  	  deletedPost.destroyRecord();
+  	  //deletedPost.save;
+  	}
+  },
   // Allows to check if the authenticated user is the owner of the post
   // There should be a way to define this more globally in order to DRY the code
   isCorrectUser: Ember.computed('post.user.id', function(){
